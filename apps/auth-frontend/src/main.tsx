c27e36956c2f70227275616d56f703865e5bad8a
Developer <developer@example.com>
2026-08-28 00:26:51 +0100

    Configure axios to use VITE_API_URL in production

--- apps/auth-frontend/src/main.tsx
+++ apps/auth-frontend/src/main.tsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import axios from 'axios'
+
+if (import.meta.env.VITE_API_URL) {
+  axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
